0e03211c5a64ee4eca3011fe45175ec0add2e3b5
YuTar2 <jmgo0625@gmail.com>
2024-09-07 11:01:51 +0900

    240907 commit

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -34,7 +34,7 @@ def sql():
 @crud.route('/users/new',methods=["GET","POST"])
 def create_user():
     form = UserForm()
-    if form.validators_on_submit():
+    if form.validate_on_submit():
         user = User(
             username = form.username.data,
             email = form.email.data,
@@ -45,3 +45,14 @@ def create_user():
         
         return redirect(url_for("crud.users"))
     return render_template("crud/create.html",form = form)
+
+@crud.route('/users')
+def users():
+    users = User.query.all()
+    return render_template('crud/index.html',users = users)
+
+@crud.route('/user/<user_id>',methods = ["GET","POST"])
+def edit_user(user_id):
+    form = UserForm()
+    user = User.query.filter_by(id=user_id).first()
+    return render_template('crud/edit.html',user=user)
